f4228c958274dcebb15b15db5032d37c1b6a4cc2
MMIK0 <35567342+MMIK0@users.noreply.github.com>
2019-08-20 19:18:31 +0300

    Source code added

---
+++ Assets/Scripts/CamScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScript : MonoBehaviour {
+     public GameObject playerShip;
+
+    private float height = 2.0f;
+    private float distance = 3.0f;
+
+    void Start()
+    {
+        transform.position = playerShip.transform.TransformPoint(0f, height, -distance);
+    }
+
+    void Update()
+    {
+        transform.position = playerShip.transform.TransformPoint(0f, height, -distance);
+        transform.rotation = playerShip.transform.rotation;
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public void ChangeScene(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+       Time.timeScale = 1f;
+    }
+    
+   
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
